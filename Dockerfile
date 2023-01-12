ac31a34232499d9c88276a3bb43b36a945b800e4
Roman Bykov <contact@h963z57.com>
2023-01-12 06:49:50 -0500

    First init

---
+++ Dockerfile
@@ -0,0 +1,63 @@
+FROM debian:latest
+
+#================== Install Depends========================
+
+RUN apt-get update \
+    && DEBIAN_FRONTEND=noninteractive \   
+        apt-get install -y \
+            rsyslog \
+            postfix-mysql \
+            dovecot-mysql \ 
+            dovecot-imapd \
+            dovecot-lmtpd \
+            opendkim \
+            opendkim-tools \
+            wget \
+            gettext-base \
+            mailutils
+
+#================== Download and confgigure mc ============
+
+RUN wget https://dl.min.io/client/mc/release/linux-amd64/mc \
+    && chmod +x mc \
+    && mv mc /usr/bin/
+
+#================== Confgigure mail user and dir ==========
+
+RUN useradd -r -u 150 -g mail -d /var/vmail -s /sbin/nologin -c "Virtual Mail User" vmail \
+    && mkdir -p /var/vmail \
+    && chmod -R 770 /var/vmail \
+    && chown -R vmail:mail /var/vmail \
+    && mkdir -p /mnt/SSL/ \
+    && mkdir -p /etc/postfix/sql \
+    && mkdir -p /etc/opendkim/keys \
+    && chown opendkim:opendkim /etc/opendkim \
+    && chmod 750 /etc/opendkim
+
+
+COPY /source/templates templates/
+COPY /source/files/master.cf /etc/postfix/master.cf
+
+COPY /source/files/10-*.conf /etc/dovecot/conf.d/
+
+# COPY /templates/10-auth.conf /etc/dovecot/conf.d/10_auth.conf
+# COPY /templates/10-mail.conf /etc/dovecot/conf.d/10-mail.conf
+# COPY /templates/10-master.conf /etc/dovecot/conf.d/10-master.conf
+# COPY /templates/10-ssl.conf /etc/dovecot/conf.d/10-ssl.conf
+
+COPY /source/files/opendkim /etc/default/opendkim
+COPY /source/files/opendkim.conf /etc/opendkim.conf
+COPY /source/files/TrustedHosts /etc/opendkim/TrustedHosts
+
+# ==================== TMP REMOVE IT ===========================
+# COPY /source/h963z57.com/fullchain.pem /mnt/SSL/fullchain.pem
+# COPY /source/h963z57.com/privkey.pem /mnt/SSL/privkey.pem
+
+#COPY /source/h963z57.com /etc/opendkim/keys/h963z57.com
+# ==================== TMP REMOVE IT ===========================
+
+COPY ./entrypoint.sh /entrypoint.sh
+RUN chmod a+x /entrypoint.sh
+ENTRYPOINT ["/entrypoint.sh"]
+
+EXPOSE 25 143 587
---
+++ entrypoint.sh
@@ -0,0 +1,125 @@
+#!/bin/bash
+
+#=================== Configuration service =========================
+
+generate_configs () {
+    echo "Generating postfix and dovecot configuration for ${EMAIL_DOMAIN}"
+
+    envsubst '\$EMAIL_DB_USER \$EMAIL_DB_PASSWORD \$EMAIL_DB_HOST \$EMAIL_DB_NAME' < /templates/mysql_virtual_alias_maps.cf.j2        > /etc/postfix/sql/mysql_virtual_alias_maps.cf
+    envsubst '\$EMAIL_DB_USER \$EMAIL_DB_PASSWORD \$EMAIL_DB_HOST \$EMAIL_DB_NAME' < /templates/mysql_virtual_domains_maps.cf.j2      > /etc/postfix/sql/mysql_virtual_domains_maps.cf
+    envsubst '\$EMAIL_DB_USER \$EMAIL_DB_PASSWORD \$EMAIL_DB_HOST \$EMAIL_DB_NAME' < /templates/mysql_virtual_mailbox_maps.cf.j2      > /etc/postfix/sql/mysql_virtual_mailbox_maps.cf
+    envsubst '\$EMAIL_DB_USER \$EMAIL_DB_PASSWORD \$EMAIL_DB_HOST \$EMAIL_DB_NAME' < /templates/mysql_virtual_alias_domain_maps.cf.j2 > /etc/postfix/sql/mysql_virtual_alias_domain_maps.cf
+    envsubst '\$EMAIL_DB_USER \$EMAIL_DB_PASSWORD \$EMAIL_DB_HOST \$EMAIL_DB_NAME' < /templates/mysql_virtual_alias_domain_mailbox_maps.cf.j2  > /etc/postfix/sql/mysql_virtual_alias_domain_mailbox_maps.cf
+    envsubst '\$EMAIL_DB_USER \$EMAIL_DB_PASSWORD \$EMAIL_DB_HOST \$EMAIL_DB_NAME' < /templates/mysql_virtual_mailbox_limit_maps.cf.j2         > /etc/postfix/sql/mysql_virtual_mailbox_limit_maps.cf
+    envsubst '\$EMAIL_DB_USER \$EMAIL_DB_PASSWORD \$EMAIL_DB_HOST \$EMAIL_DB_NAME' < /templates/mysql_virtual_alias_domain_catchall_maps.cf.j2 > /etc/postfix/sql/mysql_virtual_alias_domain_catchall_maps.cf
+    
+    envsubst '\$EMAIL_DB_USER \$EMAIL_DB_PASSWORD \$EMAIL_DB_HOST \$EMAIL_DB_NAME' < /templates/dovecot-sql.conf.ext.j2 > /etc/dovecot/dovecot-sql.conf.ext
+}
+
+configuration_main_cf () {
+    echo "Confgiguration main.cf"
+
+    postconf -e "myhostname = ${EMAIL_HOSTNAME}"
+    postconf -e "mydestination = localhost"
+    postconf -e "mynetworks = ${EMAIL_NETWORKS}" 
+    postconf -e "inet_protocols = ipv4"
+    postconf -e "inet_interfaces = all"
+    postconf -e "smtpd_tls_cert_file = /mnt/SSL/fullchain.pem" 
+    postconf -e "smtpd_tls_key_file = /mnt/SSL/privkey.pem"
+    postconf -e "smtpd_tls_auth_only = yes"
+    postconf -e "smtpd_sasl_type = dovecot"
+    postconf -e "smtpd_sasl_path = private/auth"
+    postconf -e "smtpd_sasl_auth_enable = yes"
+    postconf -e "smtpd_recipient_restrictions = permit_sasl_authenticated, permit_mynetworks, reject_unauth_destination"
+    postconf -e "virtual_transport = lmtp:unix:private/dovecot-lmtp"
+    postconf -e "virtual_mailbox_domains = proxy:mysql:/etc/postfix/sql/mysql_virtual_domains_maps.cf"
+    postconf -e "virtual_alias_maps = proxy:mysql:/etc/postfix/sql/mysql_virtual_alias_maps.cf, proxy:mysql:/etc/postfix/sql/mysql_virtual_alias_domain_maps.cf, proxy:mysql:/etc/postfix/sql/mysql_virtual_alias_domain_catchall_maps.cf"
+    postconf -e "virtual_mailbox_maps = proxy:mysql:/etc/postfix/sql/mysql_virtual_mailbox_maps.cf, proxy:mysql:/etc/postfix/sql/mysql_virtual_alias_domain_mailbox_maps.cf"
+
+    echo "milter_protocol = 2"                      >> /etc/postfix/main.cf
+    echo "milter_default_action = accept"           >> /etc/postfix/main.cf
+    echo "smtpd_milters = inet:localhost:12301"     >> /etc/postfix/main.cf
+    echo "non_smtpd_milters = inet:localhost:12301" >> /etc/postfix/main.cf
+}
+
+generate_opendkim_key () {
+    echo "Start opendkim keygen..."
+    mkdir -p /etc/opendkim/keys/${EMAIL_DOMAIN}
+    opendkim-genkey -D /etc/opendkim/keys/${EMAIL_DOMAIN}/ --domain ${EMAIL_DOMAIN} --selector mail
+    echo "DKIM DNS entry:"
+    echo "========================================"
+    cat "/etc/opendkim/keys/${EMAIL_DOMAIN}/mail.txt"
+    echo "========================================"
+    chmod 600 /etc/opendkim/keys/${EMAIL_DOMAIN}/mail.private
+    echo "*.${EMAIL_DOMAIN}" >> /etc/opendkim/TrustedHosts
+    echo "mail._domainkey.${EMAIL_DOMAIN} ${EMAIL_DOMAIN}:mail:/etc/opendkim/${EMAIL_DOMAIN}/mail.private" >> /etc/opendkim/KeyTable
+    echo "*@${EMAIL_DOMAIN} mail._domainkey.${EMAIL_DOMAIN}" >> /etc/opendkim/SigningTable
+}
+
+#==================== Check env exists ============================
+if [[ -z "${EMAIL_DOMAINS}" && "${EMAIL_DB_USER}" && "${EMAIL_DB_PASSWORD}" && "${EMAIL_DB_HOST}" && "${EMAIL_DB_NAME}" && "${EMAIL_HOSTNAME}" ]]; then
+  echo "No one or more env"
+  exit 0
+else
+  echo "All env is exists"
+fi
+
+
+#============== TMP DELETE IT ===================
+generate_configsl
+configuration_main_cf
+# generate_opendkim_key
+#============== TMP DELETE IT ===================
+
+
+
+echo "Setup files priveleges"
+chown -R vmail:dovecot /etc/dovecot
+chmod -R o-rwx /etc/dovecot
+
+echo "Create conf files opendkim"
+touch /etc/opendkim/SigningTable
+touch /etc/opendkim/KeyTable
+
+
+#================= DEV AREA =======================
+
+
+
+
+
+
+
+
+if [ -d "/etc/opendkim/keys" ]
+then
+	if [ "$(ls -A /etc/opendkim/keys)" ]; then
+     echo "Take action /etc/opendkim/keys is not Empty"
+	else
+        for EMAIL_DOMAIN in $EMAIL_DOMAINS
+        do
+            generate_opendkim_key
+        done
+	fi
+else
+	echo "Directory /etc/opendkim/keys not found."
+    exit 0
+fi
+
+
+#================= DEV AREA END ====================
+
+
+
+
+#===================== Start service ====================
+
+echo "Start postfix dovecot opendkim"
+service rsyslog start
+service postfix start
+service dovecot start
+service opendkim start
+# Enable log
+touch /var/log/mail.err
+echo "Log started..."
+tail -f /var/log/mail.err
